9d8487c40c996ea0ef229918b284b1b28d3537fe
jzw <zhengwei.jin@gmail.com>
2014-01-08 18:52:04 +0800

    新增doc.tests项目，并将之前写的doc相关的测试用例从drip中迁移过来

---
+++ server/tests/com.zizibujuan.drip.server.doc.tests/src/com/zizibujuan/drip/server/doc/tests/AllServletTests.java
@@ -0,0 +1,15 @@
+package com.zizibujuan.drip.server.doc.tests;
+
+/**
+ * Servlet测试套件
+ * @author jzw
+ * @since 0.0.1
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+	ProjectServletTests.class,
+	FileServletTests.class
+})
+public class AllServletTests {
+
+}
