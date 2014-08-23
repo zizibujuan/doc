b75511880607b4be6e55a88884ba7928d2ef27b6
jinzw <zhengwei.jin@gmail.com>
2014-08-23 22:50:15 +0800

    完善Activator类

--- server/bundles/com.zizibujuan.drip.server.doc.configurator/src/com/zizibujuan/drip/server/doc/configurator/Activator.java
+++ server/bundles/com.zizibujuan.drip.server.doc.configurator/src/com/zizibujuan/drip/server/doc/configurator/Activator.java
@@ -1,14 +1,24 @@
 package com.zizibujuan.drip.server.doc.configurator;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.packageadmin.PackageAdmin;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+	static Activator singleton;
+	// TODO:替换到PackageAdmin
+	private ServiceTracker<PackageAdmin, PackageAdmin> packageAdminTracker;
 
 	static BundleContext getContext() {
 		return context;
+	}
+	
+	public static Activator getDefault() {
+		return singleton;
 	}
 
 	/*
@@ -16,7 +26,11 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
+		singleton = this;
 		Activator.context = bundleContext;
+		packageAdminTracker = new ServiceTracker<PackageAdmin, PackageAdmin>(context, PackageAdmin.class.getName(), null);
+		packageAdminTracker.open();
+		
 	}
 
 	/*
@@ -25,6 +39,27 @@ public class Activator implements BundleActivator {
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+		
+		if (packageAdminTracker != null) {
+			packageAdminTracker.close();
+			packageAdminTracker = null;
+		}
+	}
+
+	protected Bundle getBundle(String symbolicName) {
+		PackageAdmin packageAdmin = packageAdminTracker.getService();
+		if (packageAdmin == null)
+			return null;
+		Bundle[] bundles = packageAdmin.getBundles(symbolicName, null);
+		if (bundles == null)
+			return null;
+		// Return the first bundle that is not installed or uninstalled
+		for (int i = 0; i < bundles.length; i++) {
+			if ((bundles[i].getState() & (Bundle.INSTALLED | Bundle.UNINSTALLED)) == 0) {
+				return bundles[i];
+			}
+		}
+		return null;
 	}
 
 }
