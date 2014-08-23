ffbccc1f3acff5345cfcd9e3adac322f0ef46846
jinzw <zhengwei.jin@gmail.com>
2014-08-23 23:03:37 +0800

    设置首页

---
+++ server/bundles/com.zizibujuan.drip.server.doc.configurator/src/com/zizibujuan/drip/server/doc/configurator/servlet/WelcomeFileFilter.java
@@ -0,0 +1,54 @@
+package com.zizibujuan.drip.server.doc.configurator.servlet;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.framework.Filter;
+
+/**
+ * 指向欢迎页面的过滤器。
+ * 当访问首页时，如果用户没有登录，则跳转到公共首页；如果用户已经登录，则跳转到个人首页。
+ * 
+ * @author jinzw
+ * @since 0.0.1
+ */
+public class WelcomeFileFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		final HttpServletRequest httpRequest = (HttpServletRequest) request;
+		final HttpServletResponse httpResponse = (HttpServletResponse)response;
+		String requestPath = httpRequest.getServletPath() + (httpRequest.getPathInfo() == null ? "" : httpRequest.getPathInfo()); //$NON-NLS-1$
+		
+		if(requestPath.equals("/index.html")){
+			requestPath = "/";
+		}
+		// 判断是否访问首页地址
+		if (requestPath.equals("/")) { //$NON-NLS-1$
+			String fileName = "";
+			fileName = requestPath + "doc/index.html";
+			httpResponse.setHeader("Cache-Control", "no-cache"); //$NON-NLS-1$ //$NON-NLS-2$
+			httpResponse.setHeader("Cache-Control", "no-store"); //$NON-NLS-1$ //$NON-NLS-2$
+			
+			httpRequest.getRequestDispatcher(fileName).forward(httpRequest, response);
+			
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// do nothing
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// do nothing
+	}
+
+}
