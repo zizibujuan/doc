a8901be8b5b2eb7dbb20cce100cc228ba6707a91
xiaohulu <idocument@qq.com>
2014-09-20 15:49:06 +0800

    在页面的顶部显示用户登录信息

---
+++ client/bundles/com.zizibujuan.drip.client.doc/web/doc/userView.js
@@ -0,0 +1,21 @@
+
+function showUserZone(){
+	// 如果用户已经登录，则显示用户信息
+	$.getJSON("/users/").done(function(data){
+		// 当前用户已登录
+		$("#linkUser").attr("href", "/users/" + data.id);
+		$("#imgUser").attr("alt", data.nickName);
+		var smallImageUrl = data.smallImageUrl;
+		if(smallImageUrl != null && smallImageUrl != ""){
+			$("#imgUser").attr("src", smallImageUrl);
+		}else{
+			$("#imgUser").attr("src", "/useradmin/resources/images/profile_50_50.gif");
+		}
+		$("#spanUser").html(data.nickName || data.loginName);
+		
+	}).fail(function(error){
+		// 当前用户没有登录
+		console.error("用户没有登录", error);
+	});
+}
+
